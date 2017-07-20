e9ede81fcb6adb9fa6ca1f9924babcc2d05fd038
rahmeen habib <rahmeenwill99@gmail.com>
2017-07-20 22:46:58 +0530

    Add some styling to version 1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds= [{name: "SIRNAS", image: "http://www.myharriman.com/wp-content/uploads/2013/07/camping_at_night.jpg"}, {name: "Dark Fortress", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"}];
+var campgrounds= [{name: "SIRNAS", image: "http://www.myharriman.com/wp-content/uploads/2013/07/camping_at_night.jpg"}, {name: "Dark Fortress", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"}, {name: "Purple hue", image: "http://g.fastcompany.net/multisite_files/fastcompany/imagecache/1280/poster/2015/02/3042691-poster-1280-tentsile.jpg"}];
 
 app.get("/", function(req, res){
     res.render("landing");
